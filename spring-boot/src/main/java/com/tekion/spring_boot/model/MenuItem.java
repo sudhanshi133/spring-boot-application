404dd03afec630e623199217a793440a8da25731
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-01-14 11:49:09 +0530

    added files

--- spring-boot/src/main/java/com/tekion/spring_boot/model/MenuItem.java
+++ spring-boot/src/main/java/com/tekion/spring_boot/model/MenuItem.java
@@ -1,17 +1,61 @@
 package com.tekion.spring_boot.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class MenuItem {
     private Long id;
     private String name;
     private String description;
     private double price;
     private String category;
+
+    public MenuItem() {
+    }
+
+    public MenuItem(Long id, String name, String description, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
 
